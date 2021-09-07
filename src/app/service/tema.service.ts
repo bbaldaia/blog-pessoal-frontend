3ddb0d0c70b10bc512f4fbd88bb44eb8e0c231e1
bbaldaia <brunobfreire98@gmail.com>
2021-09-06 22:36:42 -0300

    Implementando o Put e o Delete

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,7 +19,19 @@ export class TemaService {
     return this.http.get<Tema[]>('https://deplobrunobaldaia.herokuapp.com/temas', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`https://deplobrunobaldaia.herokuapp.com/temas/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>('https://deplobrunobaldaia.herokuapp.com/temas', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://deplobrunobaldaia.herokuapp.com/temas', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://deplobrunobaldaia.herokuapp.com/temas/${id}`, this.token)
   }
 }
